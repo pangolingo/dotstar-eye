1eb2cbc825ca4639872135a5ec02e5545a4f24da
Dave Iverson <dave@ivrsn.com>
2018-03-27 22:08:12 -0500

    added Dotstar functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import keyboard
 import argparse
+import apa102
 
 # --headless will run without window
 parser = argparse.ArgumentParser()
@@ -15,6 +16,10 @@ import pygame
 
 
 run_loop = True
+
+# Initialize the library and the strip
+strip = apa102.APA102(num_led=64, global_brightness=1, mosi = 10, sclk = 11,
+                                          order='rgb')
 
 
 class EyeLid(pygame.sprite.Sprite):
@@ -51,6 +56,15 @@ class EyeLid(pygame.sprite.Sprite):
         self.blinking_closing = True
 
 
+def draw_to_dotstar(dotstar, image):
+    pixel_num = 0
+    for row in image:
+        for pixel in row:
+            dotstar.set_pixel(int(pixel_num), int(pixel[0]), int(pixel[1]), int(pixel[2]))
+            pixel_num += 1
+
+    dotstar.show()
+
 def main():
 
     def move(dir):
@@ -73,99 +87,112 @@ def main():
     def blink():
         return None
 
+    # Turn off all pixels (sometimes a few light up when the strip gets power)
+    strip.clear_strip()
 
     """ Set up the game and run the main game loop """
     pygame.init()      # Prepare the pygame module for use
     pygame.display.init()
-    screen_size = 8   # Desired physical surface size, in pixels.
-    preview_size = 64
-
-    # Create surface of (width, height), and its window.
-    viewable_surface = pygame.display.set_mode((preview_size, preview_size), 0, 32 if args.headless else 0)
-    main_surface = pygame.surface.Surface((screen_size, screen_size))
-
-    clock = pygame.time.Clock()
 
 
-    eye_white = pygame.image.load("images/eye_white.png")
-    eye_ball = pygame.image.load("images/eye_ball.png")
-    eye_alpha_overlay = pygame.image.load("images/eye_alpha_overlay.png")
-    eye_mask = pygame.image.load("images/eye_mask.png")
-    # eye_lid = pygame.image.load("images/eye_lid.png")
+    try:
+
+        screen_size = 8   # Desired physical surface size, in pixels.
+        preview_size = 64
+
+        # Create surface of (width, height), and its window.
+        viewable_surface = pygame.display.set_mode((preview_size, preview_size), 0, 32 if args.headless else 0)
+        main_surface = pygame.surface.Surface((screen_size, screen_size))
+
+        clock = pygame.time.Clock()
 
 
-    eye_ball_pos = [0, 0]
-    # eye_lid_pos = [0, 0 - eye_lid.get_size()[1] / 2] # start offscreen
+        eye_white = pygame.image.load("images/eye_white.png")
+        eye_ball = pygame.image.load("images/eye_ball.png")
+        eye_alpha_overlay = pygame.image.load("images/eye_alpha_overlay.png")
+        eye_mask = pygame.image.load("images/eye_mask.png")
+        # eye_lid = pygame.image.load("images/eye_lid.png")
 
-    
-    all_sprites = pygame.sprite.Group()
-    eyelid = EyeLid()
-    all_sprites.add(eyelid)
-    
 
-    def headless_key_pressed(event, scan_code=None, time=None):
-        global run_loop
-        if event.name == 'esc':         run_loop = False
-        elif event.name == 'up':        move("up")
-        elif event.name == 'down':      move("down")
-        elif event.name == 'left':      move("left")
-        elif event.name == 'right':     move("right")
-        elif event.name == 'space':     blink()
-    
-    if args.headless:
-        keyboard.on_press(headless_key_pressed)
-
-    while run_loop:
-        if pygame.event.get(pygame.QUIT): break
-        pygame.event.pump()
-
-        if not args.headless:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_ESCAPE]: break
-            if keys[pygame.K_UP]:       move("up")
-            if keys[pygame.K_DOWN]:     move("down")
-            if keys[pygame.K_LEFT]:     move("left")
-            if keys[pygame.K_RIGHT]:    move("right")
-            if keys[pygame.K_SPACE]:      eyelid.blink()
+        eye_ball_pos = [0, 0]
+        # eye_lid_pos = [0, 0 - eye_lid.get_size()[1] / 2] # start offscreen
 
         
+        all_sprites = pygame.sprite.Group()
+        eyelid = EyeLid()
+        all_sprites.add(eyelid)
+        
+
+        def headless_key_pressed(event, scan_code=None, time=None):
+            global run_loop
+            if event.name == 'esc':         run_loop = False
+            elif event.name == 'up':        move("up")
+            elif event.name == 'down':      move("down")
+            elif event.name == 'left':      move("left")
+            elif event.name == 'right':     move("right")
+            elif event.name == 'space':     blink()
+        
+        if args.headless:
+            keyboard.on_press(headless_key_pressed)
+
+        while run_loop:
+            if pygame.event.get(pygame.QUIT): break
+            pygame.event.pump()
+
+            if not args.headless:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_ESCAPE]: break
+                if keys[pygame.K_UP]:       move("up")
+                if keys[pygame.K_DOWN]:     move("down")
+                if keys[pygame.K_LEFT]:     move("left")
+                if keys[pygame.K_RIGHT]:    move("right")
+                if keys[pygame.K_SPACE]:      eyelid.blink()
+
+            
+                
+
             
 
-        
+            main_surface.fill((0, 0, 0))
+            viewable_surface.fill((0, 0, 0))
 
-        main_surface.fill((0, 0, 0))
-        viewable_surface.fill((0, 0, 0))
+            main_surface.blit(eye_white, (0, 0))
+            main_surface.blit(eye_ball, eye_ball_pos)
+            main_surface.blit(eye_alpha_overlay, (0, 0))
 
-        main_surface.blit(eye_white, (0, 0))
-        main_surface.blit(eye_ball, eye_ball_pos)
-        main_surface.blit(eye_alpha_overlay, (0, 0))
+            all_sprites.update()
+            all_sprites.draw(main_surface)
 
-        all_sprites.update()
-        all_sprites.draw(main_surface)
-
-        main_surface.blit(eye_mask, (0, 0))
-        # main_surface.blit(eye_lid, (0, 0))
+            main_surface.blit(eye_mask, (0, 0))
+            # main_surface.blit(eye_lid, (0, 0))
 
 
 
-        # Overpaint a smaller rectangle on the main surface
-        # main_surface.fill((255, 0, 0), (0, 0, 2, 2))
-        # main_surface.fill((0, 255, 0), (6, 0, 2, 2))
-        
+            # Overpaint a smaller rectangle on the main surface
+            # main_surface.fill((255, 0, 0), (0, 0, 2, 2))
+            # main_surface.fill((0, 255, 0), (6, 0, 2, 2))
 
-        viewable_surface.blit(pygame.transform.scale(main_surface, (64, 64)), (0, 0))
+            
 
-        # numpy arr
-        # print(pygame.surfarray.array3d(main_surface))
-        # print(pygame.surfarray.array3d(eye_ball)[0])
-        # print(pygame.surfarray.array3d(main_surface)[0][5])
+            viewable_surface.blit(pygame.transform.scale(main_surface, (64, 64)), (0, 0))
+
+            # numpy arr
+            # print(pygame.surfarray.array3d(main_surface))
+            # print(pygame.surfarray.array3d(eye_ball)[0])
+            # print(pygame.surfarray.array3d(main_surface)[0][5])
+            if args.headless:
+                draw_to_dotstar(strip, pygame.surfarray.array3d(main_surface))
 
 
-        # Now the surface is ready, tell pygame to display it!
-        pygame.display.flip()
+            # Now the surface is ready, tell pygame to display it!
+            pygame.display.flip()
 
-        clock.tick(10)
+            clock.tick(10)
 
-    pygame.quit()     # Once we leave the loop, close the window.
+    finally:
+        pygame.quit()     # Once we leave the loop, close the window.
+        # strip cleanup
+        strip.clear_strip()
+        strip.cleanup()
 
 main()
